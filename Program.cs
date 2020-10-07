476f05bf2218d769e1d46cb1493a17df4f375fc8
AkshayPoriya <akshayporiya1234@gmail.com>
2020-10-07 08:45:38 +0530

    Modified menthod name

--- Program.cs
+++ Program.cs
@@ -18,17 +18,17 @@ namespace _08_UserRegistrationProblem
                     case "1":
                         Console.WriteLine("Enter First Name");
                         string firstName = Console.ReadLine();
-                        Console.WriteLine("{0}",isValidFirstName(firstName)?"Valid":"Invalid"); // Ternary operator
+                        Console.WriteLine("{0}",IsValidFirstName(firstName)?"Valid":"Invalid"); // Ternary operator
                         break;
                     case "2":
                         Console.WriteLine("Enter Last Name");
                         string lastName = Console.ReadLine();
-                        Console.WriteLine("{0}", isValidLastName(lastName) ? "Valid" : "Invalid"); // Ternary operator
+                        Console.WriteLine("{0}", IsValidLastName(lastName) ? "Valid" : "Invalid"); // Ternary operator
                         break;
                     case "3":
                         Console.WriteLine("Enter Email");
                         string email = Console.ReadLine();
-                        Console.WriteLine("{0}", isValidEmail(email) ? "Valid" : "Invalid"); // Ternary operator
+                        Console.WriteLine("{0}", IsValidEmail(email) ? "Valid" : "Invalid"); // Ternary operator
                         break;
                     default:
                         flag = false; // To break loop
@@ -38,7 +38,7 @@ namespace _08_UserRegistrationProblem
             }
         }
 
-        static bool isValidFirstName(string firstName)
+        static bool IsValidFirstName(string firstName)
         {
             string pattern = @"^[A-Z][a-z][a-z]+$"; //Pattern for Capital letter followed by small letters, min length 3
             Regex regex = new Regex(pattern);
@@ -49,7 +49,7 @@ namespace _08_UserRegistrationProblem
             return false;
         }
 
-        static bool isValidLastName(string lastName)
+        static bool IsValidLastName(string lastName)
         {
             string pattern = @"^[A-Z][a-z][a-z]+$"; //Pattern for Capital letter followed by small letters, min length 3
             Regex regex = new Regex(pattern);
@@ -60,7 +60,7 @@ namespace _08_UserRegistrationProblem
             return false;
         }
 
-        static bool isValidEmail(string email)
+        static bool IsValidEmail(string email)
         {
             string pattern = @"^[a-zA-Z0-9]+[.A-Za-z0-9]*[@][a-zA-Z]+[.][a-zA-Z]+[.a-zA-Z]*$"; //Pattern for Capital letter followed by small letters, min length 3
             Regex regex = new Regex(pattern);
